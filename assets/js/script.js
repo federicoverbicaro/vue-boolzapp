0e0d18ab553e8ada106e03d726a4c035de9e29d1
federicoverbicaro <federico.verbicaro@icloud.com>
2024-06-14 15:15:29 +0200

    fix Eliminazione messaggio

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,16 +16,19 @@ createApp({
                     visible: true,
                     messages: [
                         {
+                            id: 1,
                             date: '10/01/2020 15:30:55',
                             message: 'Hai portato a spasso il cane?',
                             status: 'sent'
                         },
                         {
+                            id: 2,
                             date: '10/01/2020 15:50:00',
                             message: 'Ricordati di stendere i panni',
                             status: 'sent'
                         },
                         {
+                            id: 3,
                             date: '10/01/2020 16:15:22',
                             message: 'Tutto fatto!',
                             status: 'received'
@@ -39,16 +42,19 @@ createApp({
                     visible: true,
                     messages: [
                         {
+                            id: 1,
                             date: '20/03/2020 16:30:00',
                             message: 'Ciao come stai?',
                             status: 'sent'
                         },
                         {
+                            id: 2,
                             date: '20/03/2020 16:30:55',
                             message: 'Bene grazie! Stasera ci vediamo?',
                             status: 'received'
                         },
                         {
+                            id: 3,
                             date: '20/03/2020 16:35:00',
                             message: 'Mi piacerebbe ma devo andare a fare la spesa.',
                             status: 'sent'
@@ -62,16 +68,19 @@ createApp({
                     visible: true,
                     messages: [
                         {
+                            id: 1,
                             date: '28/03/2020 10:10:40',
                             message: 'La Marianna va in campagna',
                             status: 'received'
                         },
                         {
+                            id: 2,
                             date: '28/03/2020 10:20:10',
                             message: 'Sicuro di non aver sbagliato chat?',
                             status: 'sent'
                         },
                         {
+                            id: 3,
                             date: '28/03/2020 16:15:22',
                             message: 'Ah scusa!',
                             status: 'received'
@@ -85,11 +94,13 @@ createApp({
                     visible: true,
                     messages: [
                         {
+                            id: 1,
                             date: '10/01/2020 15:30:55',
                             message: 'Lo sai che ha aperto una nuova pizzeria?',
                             status: 'sent'
                         },
                         {
+                            id: 2,
                             date: '10/01/2020 15:50:00',
                             message: 'Si, ma preferirei andare al cinema',
                             status: 'received'
@@ -103,11 +114,13 @@ createApp({
                     visible: true,
                     messages: [
                         {
+                            id: 1,
                             date: '10/01/2020 15:30:55',
                             message: 'Ricordati di chiamare la nonna',
                             status: 'sent'
                         },
                         {
+                            id: 2,
                             date: '10/01/2020 15:50:00',
                             message: 'Va bene, stasera la sento',
                             status: 'received'
@@ -121,16 +134,19 @@ createApp({
                     visible: true,
                     messages: [
                         {
+                            id: 1,
                             date: '10/01/2020 15:30:55',
                             message: 'Ciao Claudia, hai novità?',
                             status: 'sent'
                         },
                         {
+                            id: 2,
                             date: '10/01/2020 15:50:00',
                             message: 'Non ancora',
                             status: 'received'
                         },
                         {
+                            id: 3,
                             date: '10/01/2020 15:51:00',
                             message: 'Nessuna nuova, buona nuova',
                             status: 'sent'
@@ -144,11 +160,13 @@ createApp({
                     visible: true,
                     messages: [
                         {
+                            id: 1,
                             date: '10/01/2020 15:30:55',
                             message: 'Fai gli auguri a Martina che è il suo compleanno!',
                             status: 'sent'
                         },
                         {
+                            id: 2,
                             date: '10/01/2020 15:50:00',
                             message: 'Grazie per avermelo ricordato, le scrivo subito!',
                             status: 'received'
@@ -162,16 +180,19 @@ createApp({
                     visible: true,
                     messages: [
                         {
+                            id: 1,
                             date: '10/01/2020 15:30:55',
                             message: 'Ciao, andiamo a mangiare la pizza stasera?',
                             status: 'received'
                         },
                         {
+                            id: 2,
                             date: '10/01/2020 15:50:00',
                             message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
                             status: 'sent'
                         },
                         {
+                            id: 3,
                             date: '10/01/2020 15:51:00',
                             message: 'OK!!',
                             status: 'received'
@@ -239,12 +260,20 @@ createApp({
               
             });
         },
-
-        eliminaMessagio(index) {
-            this.contacts[this.utente].messages.splice(index, 1);
-
-        },
-
+        eliminaMessaggio(messageId) {
+            // Cerca il messaggio con l'id specificato nel contatto corrente
+            let contatto = this.contacts[this.utente];
+            let messageIndex = contatto.messages.findIndex(messaggio => messaggio.id === messageId);
+        
+            if (messageIndex !== -1) {
+                // Rimuovi il messaggio dall'array 'messages' del contatto corrente
+                contatto.messages.splice(messageIndex, 1);
+                console.log(`Messaggio con id ${messageId} eliminato.`);
+                console.log('Nuovi messaggi:', contatto.messages);
+            } else {
+                console.error('Messaggio non trovato');
+            }
+        }
        
 
     }
